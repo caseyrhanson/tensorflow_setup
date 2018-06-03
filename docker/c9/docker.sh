72b897fe18b4ddb371fdd40c91a6f839ff53b38f
Casey Hanson <caseyrhanson>
2018-06-03 18:14:05 -0500

    message:

--- docker/c9/docker.sh
+++ docker/c9/docker.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-## Just some comment
 IMAGE="caseyrhanson/opal_c9:1.0"
-C9PASS="t"
+C9PASS="tmppass"
 USERS="crhanso2 blatti"
 UPORT=8081
 DOCKER_VERBOSE="yes"
@@ -11,16 +10,17 @@ DOCKER_VERBOSE="yes"
 OPTIONS="-d --restart=always --privileged";
 VOLUMES="-v /var/run/docker.sock:/var/run/docker.sock";
 VOLUMES+=" -v $(which docker):$(which docker)";
-VOLUMES+=" -v /workspace/shared:/workspace/shared";
+VOLUMES+=" -v /home/:/workspace/home/";
+VOLUMES+=" -v /mnt/:/workspace/mnt/";
 
 ## Iterate over all users - in this case crhanso2 and blatti
 for USER in $USERS; do
   ## Set up USER arguments
-  USER_VOLUMES=" -v /workspace/$USER:/workspace $VOLUMES";
+  USER_VOLUMES=" -v /home/$USER/:/workspace/ $VOLUMES";
   USER_OPTIONS="$OPTIONS --name c9-$USER -h c9-$USER -p $UPORT:8181 $USER_VOLUMES";
 	COMMAND="--auth $USER:$C9PASS";
   
-  ## Run verbose 
+  ## Run verbose  output
   if [[ ! -z "$DOCKER_VERBOSE" ]]; then
     echo "docker run $USER_OPTIONS $IMAGE $COMMAND";
   fi
@@ -28,6 +28,6 @@ for USER in $USERS; do
   ## Run command
   docker run $USER_OPTIONS $IMAGE $COMMAND;
   
-  ## Increment UPORT
+  ## Increment UPORT for next user
   UPORT=`echo $UPORT | awk '{s=$1+1; print s}'`;
  done
